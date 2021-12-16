638ee6e7de1db317d27a7c0f9e70a1ddc4838a59
Julian Grove <julian.grove@gmail.com>
2021-12-16 14:01:40 +0100

    Organize directory.

---
+++ src/TLC/RN.hs
@@ -0,0 +1,103 @@
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE LambdaCase #-}
+
+module TLC.RN where
+
+import Control.Monad (ap)
+
+data Distribution = Bernoulli RN | Normal RN RN deriving Show
+
+data V = V Char Int
+
+instance Enum V where
+  succ (V c i) = case c of
+                     'x' -> V 'y' i
+                     'y' -> V 'z' i
+                     'z' -> V 'u' i
+                     'u' -> V 'v' i
+                     'v' -> V 'w' i
+                     'w' -> V 'x' (succ i)
+  toEnum i = case mod i 6 of
+               0 -> V 'x' (div i 6)
+               1 -> V 'y' (div i 6)
+               2 -> V 'z' (div i 6)
+               3 -> V 'u' (div i 6)
+               4 -> V 'v' (div i 6)
+               5 -> V 'w' (div i 6)
+  fromEnum (V c i) = case c of
+                         'x' -> i*6
+                         'y' -> i*6 + 1
+                         'z' -> i*6 + 2
+                         'u' -> i*6 + 3
+                         'v' -> i*6 + 4
+                         'w' -> i*6 + 5
+
+instance Show V where
+  show (V c i) = if i == 0 then [c] else c : show i
+
+data UnOp = Neg | Dirac
+data BinOp = Add | Sub | Mul | Equal
+
+data RN = Lit Double
+          | UnOp UnOp RN
+          | BinOp BinOp RN RN
+          | Integral Distribution RN RN (V -> RN)
+
+helpShow :: RN -> V -> String
+helpShow (Lit d) i = show d
+helpShow (UnOp Neg x) i = "-" ++ helpShow x i
+helpShow (UnOp Dirac x) i = "δ(" ++ helpShow x i ++ ")"
+helpShow (BinOp Add x y) i = "(" ++ helpShow x i ++ " + " ++ helpShow y i ++ ")"
+helpShow (BinOp Mul x y) i = "(" ++ helpShow x i ++ " - " ++ helpShow y i ++ ")"
+helpShow (BinOp Sub x y) i = "(" ++ helpShow x i ++ " * " ++ helpShow y i ++ ")"
+helpShow (BinOp Equal x y) i = "(" ++ helpShow x i ++ " = " ++ helpShow y i ++ ")"
+helpShow (Integral distr x y f) i = show distr ++ "(" ++ helpShow x i ++ ", " ++ helpShow y i ++ ")" ++ show i ++ ":" ++ "(" ++ helpShow (f i) (succ i) ++ ")"
+
+instance Show RN where
+  show x = helpShow x (toEnum 0)
+
+rToDouble :: RN -> Double
+rToDouble (Lit x) = x
+rToDouble (UnOp Neg x) = -(rToDouble x)
+rToDouble (BinOp Add x y) = rToDouble x + rToDouble y
+rToDouble (BinOp Mul x y) = rToDouble x * rToDouble y
+rToDouble (BinOp Sub x y) = rToDouble x - rToDouble y
+
+instance Num RN where
+  x + y = BinOp Add x y
+  x * y = BinOp Mul x y
+  abs = \case
+    Lit x -> Lit x
+    UnOp Neg x -> abs x
+    UnOp Dirac x -> UnOp Dirac (abs x)
+    BinOp Add x y -> BinOp Add (abs x) (abs y)
+    BinOp Mul x y -> BinOp Mul (abs x) (abs y)
+    x -> x
+  signum = \case
+    UnOp Dirac x -> Lit 1
+    x -> Lit $ rToDouble x
+  fromInteger x = Lit (fromInteger x)
+  negate = UnOp Neg
+ 
+data PP a = PP { expect :: (a -> RN) -> RN }
+
+instance Functor PP where
+  fmap f (PP m) = PP $ \k -> m (k . f)
+instance Applicative PP where
+  pure = PP . flip ($)
+  (<*>) = ap
+instance Monad PP where
+  return x = PP (\k -> k x)
+  (PP a) >>= f = PP (\k -> a $ \x -> expect (f x) k) 
+
+pf :: Sampleable a => PP a -> a -> RN
+pf (PP p) y = p (equal y)
+
+class Sampleable a where
+  equal :: a -> a -> RN
+
+instance Sampleable RN where
+  equal x y = UnOp Dirac (x - y)
+
+instance (Sampleable a, Sampleable b) => Sampleable (a, b) where
+  equal (x1, x2) (y1, y2) = equal x1 y1 * equal x2 y2
---
+++ src/TLC/Terms.hs
@@ -0,0 +1,159 @@
+{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE UnicodeSyntax #-}
+
+module TLC.Terms where
+
+import Data.Functor.Identity
+import Prelude
+import RN
+
+data Type = E | T | R | U | Γ
+          | Type :-> Type
+          | Unit
+          | Type :× Type
+
+data (α :: Type) ∈ (γ :: Type) where
+  Get :: α ∈ (α × γ)
+  Weaken :: α ∈ γ -> α ∈ (β × γ)
+deriving instance Show (α ∈ γ)
+
+type α × β = α ':× β
+type α ⟶ β = α ':-> β
+
+data Con α where
+  RN :: RN -> Con R
+  Height :: Con (E ⟶ R)
+  Human :: Con (E ⟶ T)
+  Theta :: Con R
+  GTE :: Con (R ⟶ R ⟶ R)
+  Empty :: Con Γ
+  Upd :: Con (E ⟶ Γ ⟶ Γ)
+  Sel :: Con (Γ ⟶ E)
+deriving instance Show (Con α)
+
+-- Well-typed terms.
+data γ ⊢ α where
+  Var :: α ∈ γ -> γ ⊢ α
+  Con :: Con α -> γ ⊢ α
+  App :: γ ⊢ (α ⟶ β) -> γ ⊢ α -> γ ⊢ β
+  Lam :: (α × γ) ⊢ β -> γ ⊢ (α ⟶ β)
+  Fst :: γ ⊢ (α × β) -> γ ⊢ α
+  Snd :: γ ⊢ (α × β) -> γ ⊢ β
+  Pair :: γ ⊢ α -> γ ⊢ β -> γ ⊢ (α × β)
+deriving instance Show (γ ⊢ t)
+
+subst :: (α × γ) ⊢ β -> γ ⊢ α -> γ ⊢ β
+subst (Var Get) t = t
+subst (Var (Weaken i)) t = Var i
+subst (Con c) t = Con c
+subst (App m n) t = App (subst m t) (subst n t)
+subst (Lam m) t = Lam $ subst (exch m) (wkn t)
+subst (Fst m) t = Fst $ subst m t
+subst (Snd m ) t = Snd $ subst m t
+subst (Pair m n) t = Pair (subst m t) (subst n t)
+
+evalstepβ :: γ ⊢ α -> γ ⊢ α
+evalstepβ (Var i) = Var i
+evalstepβ (App m n) = case m of
+                        Lam m' -> subst m' n
+                        _ -> App (evalstepβ m) (evalstepβ n)
+evalstepβ (Lam m) = Lam $ evalstepβ m
+evalstepβ (Fst m) = case m of
+                      Pair m' n' -> evalstepβ m'
+                      _ -> Fst $ evalstepβ m
+evalstepβ (Snd m) = case m of
+                      Pair m' n' -> evalstepβ n'
+                      _ -> Snd $ evalstepβ m
+evalstepβ (Pair m n) = Pair (evalstepβ m) (evalstepβ n)
+
+normalF :: γ ⊢ α -> Bool
+normalF (Var i) = True
+normalF (Con c) = True
+normalF (App m n) = case m of
+                      Lam m' -> False
+                      _-> normalF m && normalF n
+normalF (Lam m) = normalF m
+normalF (Fst m) = case m of
+                    Pair m' n' -> False
+                    _ -> normalF m
+normalF (Snd m) = case m of
+                    Pair m' n' -> False
+                    _ -> normalF m
+normalF (Pair m n) = normalF m && normalF n
+
+evalβ :: γ ⊢ α -> γ ⊢ α
+evalβ m = if normalF m then m else evalβ (evalstepβ m)
+
+lft :: (α ∈ γ -> α ∈ δ) -> α ∈ (β × γ) -> α ∈ (β × δ)
+lft f Get = Get
+lft f (Weaken i) = Weaken $ f i
+
+π :: α ∈ κ -> γ ⊢ κ -> γ ⊢ α
+π Get γ = Fst γ
+π (Weaken i) γ = π i (Snd γ)
+
+type Context
+  = (E ⟶ R × (E ⟶ T × (R × ((R ⟶ R ⟶ R) × (Γ × ((E ⟶ Γ ⟶ Γ) × ((Γ ⟶ E) × Unit)))))))
+
+findC :: Con α -> α ∈ Context
+findC Height = Get
+findC Human = Weaken Get
+findC Theta = Weaken (Weaken Get)
+findC GTE = Weaken (Weaken (Weaken Get))
+findC Empty = Weaken (Weaken (Weaken (Weaken Get)))
+findC Upd = Weaken (Weaken (Weaken (Weaken (Weaken Get))))
+findC Sel = Weaken (Weaken (Weaken (Weaken (Weaken (Weaken Get)))))
+
+rename :: (∀α. α ∈ γ -> α ∈ δ) -> γ ⊢ β -> δ ⊢ β
+rename f (Var i) = Var $ f i
+rename f (Con c) = (Con c)
+rename f (App m n) = App (rename f m) (rename f n)
+rename f (Lam m) = Lam $ rename (lft f) m
+rename f (Fst m) = Fst $ rename f m
+rename f (Snd m) = Snd $ rename f m
+rename f (Pair m n) = Pair (rename f m) (rename f n)
+
+wkn :: γ ⊢ α -> (β × γ) ⊢ α
+wkn = rename Weaken
+
+exch :: (α × (β × γ)) ⊢ ω -> (β × (α × γ)) ⊢ ω
+exch = rename $ \case
+  Get -> Weaken Get
+  Weaken Get -> Get
+
+contr :: (α × (α × γ)) ⊢ β -> (α × γ) ⊢ β
+contr = rename $ \case
+  Get -> Get
+  Weaken i -> i
+
+hmorph0 :: γ ⊢ α -> (Context × γ) ⊢ α
+hmorph0 (Var i) = Var (Weaken i)
+hmorph0 (App m n) = App (hmorph0 m) (hmorph0 n)
+hmorph0 (Lam m) = Lam $ exch (hmorph0 m)
+hmorph0 (Fst m) = Fst $ hmorph0 m
+hmorph0 (Snd m) = Snd $ hmorph0 m
+hmorph0 (Pair m n) = Pair (hmorph0 m) (hmorph0 n)
+hmorph0 (Con c) = π (findC c) (Var Get)
+
+hmorph :: γ ⊢ α -> γ ⊢ (Context ⟶ α)
+hmorph m = Lam (hmorph0 m)
+
+-- >>> evalβ $ hmorph (Lam (App (Lam (App (Con Height) (Var Get))) (Var Get)))
+-- Lam (Lam (App (Fst (Var (Weaken Get))) (Var Get)))
+  
