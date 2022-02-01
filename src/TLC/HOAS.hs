8fe08004b924795e8e0e22c8a219ed0708914c48
Jean-Philippe Bernardy <jeanphilippe.bernardy@gmail.com>
2022-02-01 09:59:57 +0100

    hoas work

--- src/TLC/HOAS.hs
+++ src/TLC/HOAS.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE ScopedTypeVariables #-}
@@ -9,8 +10,10 @@
 
 module TLC.HOAS where
 
+import Prelude hiding ((>>), Num(..), (/))
+import Algebra.Classes
 import qualified TLC.Terms as F
-import TLC.Terms (type (⊢), type(∈)(..), type (×),  type (⟶))
+import TLC.Terms (type (⊢), type(∈)(..), type (×),  type (⟶), Type(..), Equality, Con(..), General(..))
 
 import Unsafe.Coerce (unsafeCoerce)
 import Type.Reflection ((:~:)(..))
@@ -58,27 +61,30 @@ fromHOAS t0 k = case t0 of
   Lam t -> F.Lam (fromHOAS (t (Var key)) (Entry key k))
              where key = newkey k
 
+fromHoas :: Exp t -> 'Unit ⊢ t
+fromHoas e = fromHOAS e Empty
+
 -- examples:
 identity :: Exp (b ⟶ b)
 identity = Lam $ \x -> x
 
 twice :: Exp (b ⟶ ((b ⟶ b) ⟶ b))
-twice = Lam $ \x -> Lam $ \f -> f `App` (f `App` x)
+twice = Lam $ \x -> Lam $ \f -> f @@ (f @@ x)
 
-infixl `App`
+infixl @@
 
 -- >>> displayVs $ fromHOAS twice Terms.HOAS.Empty
 -- (λx.(λy.y(y(x))))
-{-
+
 toHOAS :: forall γ t. γ ⊢ t -> Exp γ -> Exp t
 toHOAS e ρ =
   let eval :: forall x. γ ⊢ x -> Exp x
-      eval t = toHOAS' t ρ
+      eval t = toHOAS t ρ
   in case e of
       F.TT -> TT
       F.Con x -> Con x
       F.Var v -> lk' v ρ
-      F.Lam t -> Lam (\a -> App (toHOAS t) (Pair ρ a))
+      F.Lam t -> Lam (\a -> (toHOAS t) (Pair ρ a))
       F.App t u -> App (eval t) (eval u)
       F.Pair t u -> Pair (eval t) (eval u)
       F.Fst t -> Fst (eval t)
@@ -91,21 +97,39 @@ lk' (F.Weaken v) t = lk' v (Fst t)
 toHOAS'' :: γ ⊢ t -> Exp (γ ⟶ t)
 toHOAS'' e = Lam (toHOAS e)
 
-toHOAS' :: 'F.Unit ⊢ t -> Exp t
-toHOAS' e = (toHOAS'' e) `App` TT
+toHoas :: 'F.Unit ⊢ t -> Exp t
+toHoas e = (toHOAS'' e) @@ TT
 
 
 η :: Exp α -> Exp ((α ⟶ r) ⟶ r)
-η m = Lam $ \f -> f `App` m
+η m = Lam $ \f -> f @@ m
 
 (⋆) :: Exp ((α ⟶ r) ⟶ r) -> Exp (α ⟶ ((β ⟶ r) ⟶ r)) -> Exp ((β ⟶ r) ⟶ r)
-m ⋆ k = Lam $ \f -> m `App` (Lam $ \x -> k `App` x `App` f)
+m ⋆ k = Lam $ \f -> m @@ (Lam $ \x -> k @@ x @@ f)
 
 (>>) :: Exp (('F.Unit ⟶ r) ⟶ r) -> Exp ((β ⟶ r) ⟶ r) -> Exp ((β ⟶ r) ⟶ r)
 m >> k = m ⋆ Lam  (\_ -> k) 
 
+(≐) :: Equality α => Exp α -> Exp α -> Exp 'R
+m ≐ n = toHoas (fromHoas m F.≐ fromHoas n)
 
--- distr :: F.Equality α => Exp ((α ⟶ 'F.R) ⟶ 'F.R) -> Exp (α ⟶ 'F.R)
--- distr p = Lam $ \x -> -- (App (wkn p) (Lam ((Var Get) ≐ (Var (Weaken Get)))) / measure (wkn p))
+(@@) :: Exp (a ⟶ b) -> Exp a -> Exp b
+(@@) = App
 
--}
+measure :: Exp ((α ⟶ 'R) ⟶ 'R) -> Exp 'R
+measure p = p @@ (Lam $ \_ -> one)
+
+distr :: Equality α => Exp ((α ⟶ 'R) ⟶ 'R) -> Exp (α ⟶ 'R)
+distr p = Lam (\x -> p @@ (Lam $ \y -> x ≐ y) / measure p)
+
+instance Additive (Exp 'R) where
+  zero = Con (General (Incl 0))
+  x + y  = Con (General Addi) `App` x `App` y
+instance AbelianAdditive (Exp 'R)
+instance Group (Exp 'R) where
+  negate = App (App (Con (General Mult)) (Con (General (Incl (-1)))))
+instance Multiplicative (Exp 'R) where
+  one = Con (General (Incl 1))
+  x * y  = Con (General Mult) `App` x `App` y
+instance Division (Exp 'R) where
+  x / y  = Con (General Divi) `App` x `App` y
