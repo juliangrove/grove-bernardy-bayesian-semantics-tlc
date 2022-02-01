323e0dee40b0513ff3cf914352de231432bce2f6
Jean-Philippe Bernardy <jeanphilippe.bernardy@gmail.com>
2022-02-01 13:05:27 +0100

    hoas complete?

--- src/TLC/HOAS.hs
+++ src/TLC/HOAS.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE KindSignatures #-}
@@ -8,12 +9,14 @@
 {-# LANGUAGE ExplicitNamespaces #-}
 {-# LANGUAGE LambdaCase #-}
 
-module TLC.HOAS where
+module TLC.HOAS (Exp(..), (@@), module TLC.Terms,
+                uniform, normal, u', (≥), measure, distr, observe, factor, fromHoas,
+                (⋆), (>>), η, toHoas, π) where
 
 import Prelude hiding ((>>), Num(..), (/))
 import Algebra.Classes
 import qualified TLC.Terms as F
-import TLC.Terms (type (⊢), type(∈)(..), type (×),  type (⟶), Type(..), Equality, Con(..), General(..))
+import TLC.Terms (type (⊢), type(∈)(..), type (×),  type (⟶), Type(..), Con(..), General(..), Special(GTE),Equality)
 
 import Unsafe.Coerce (unsafeCoerce)
 import Type.Reflection ((:~:)(..))
@@ -47,7 +50,7 @@ lk :: Ctx γ -> Key x -> x ∈ γ
 lk (Entry k ρ) k' = case compareKeys k k' of
                       Just Refl -> F.Get
                       Nothing -> F.Weaken (lk ρ k')
-lk Empty _ = error "fromHOAS: panic"
+lk Empty _ = error "fromHOAS: panic: variable not found"
 
 fromHOAS :: Exp t -> Ctx γ -> γ ⊢ t
 fromHOAS t0 k = case t0 of
@@ -68,12 +71,10 @@ fromHoas e = fromHOAS e Empty
 identity :: Exp (b ⟶ b)
 identity = Lam $ \x -> x
 
-twice :: Exp (b ⟶ ((b ⟶ b) ⟶ b))
+twice :: Exp (b ⟶ (b ⟶ b) ⟶ b)
 twice = Lam $ \x -> Lam $ \f -> f @@ (f @@ x)
 
-infixl @@
-
--- >>> displayVs $ fromHOAS twice Terms.HOAS.Empty
+-- >>> F.displayVs $ fromHOAS twice Empty
 -- (λx.(λy.y(y(x))))
 
 toHOAS :: forall γ t. γ ⊢ t -> Exp γ -> Exp t
@@ -94,42 +95,70 @@ lk' :: x ∈ γ -> Exp γ -> Exp x
 lk' F.Get t = Snd t
 lk' (F.Weaken v) t = lk' v (Fst t)
 
-toHOAS'' :: γ ⊢ t -> Exp (γ ⟶ t)
-toHOAS'' e = Lam (toHOAS e)
-
 toHoas :: 'F.Unit ⊢ t -> Exp t
-toHoas e = (toHOAS'' e) @@ TT
+toHoas e = toHOAS e TT
 
 
 η :: Exp α -> Exp ((α ⟶ r) ⟶ r)
 η m = Lam $ \f -> f @@ m
 
-(⋆) :: Exp ((α ⟶ r) ⟶ r) -> Exp (α ⟶ ((β ⟶ r) ⟶ r)) -> Exp ((β ⟶ r) ⟶ r)
-m ⋆ k = Lam $ \f -> m @@ (Lam $ \x -> k @@ x @@ f)
+-- (⋆) :: Exp ((α ⟶ r) ⟶ r) -> Exp (α ⟶ ((β ⟶ r) ⟶ r)) -> Exp ((β ⟶ r) ⟶ r)
+-- m ⋆ k = Lam $ \f -> m @@ (Lam $ \x -> k @@ x @@ f)
+
+(⋆) :: Exp ((α ⟶ r) ⟶ r) -> (Exp α -> Exp ((β ⟶ r) ⟶ r)) -> Exp ((β ⟶ r) ⟶ r)
+m ⋆ k = Lam $ \f -> m @@ (Lam $ \x -> k x @@ f)
 
 (>>) :: Exp (('F.Unit ⟶ r) ⟶ r) -> Exp ((β ⟶ r) ⟶ r) -> Exp ((β ⟶ r) ⟶ r)
-m >> k = m ⋆ Lam  (\_ -> k) 
+m >> k = m ⋆ (\_ -> k) 
 
 (≐) :: Equality α => Exp α -> Exp α -> Exp 'R
-m ≐ n = toHoas (fromHoas m F.≐ fromHoas n)
+m ≐ n = Con (General EqGen) @@ m @@ n
 
+infixl @@
 (@@) :: Exp (a ⟶ b) -> Exp a -> Exp b
 (@@) = App
 
 measure :: Exp ((α ⟶ 'R) ⟶ 'R) -> Exp 'R
 measure p = p @@ (Lam $ \_ -> one)
 
-distr :: Equality α => Exp ((α ⟶ 'R) ⟶ 'R) -> Exp (α ⟶ 'R)
-distr p = Lam (\x -> p @@ (Lam $ \y -> x ≐ y) / measure p)
+distr :: Equality α => Exp ((α ⟶ 'R) ⟶ 'R) -> Exp α -> Exp 'R
+distr p x = p @@ (Lam $ \y -> x ≐ y) / measure p
 
 instance Additive (Exp 'R) where
   zero = Con (General (Incl 0))
-  x + y  = Con (General Addi) `App` x `App` y
+  x + y  = Con (General Addi) @@ x @@ y
 instance AbelianAdditive (Exp 'R)
 instance Group (Exp 'R) where
   negate = App (App (Con (General Mult)) (Con (General (Incl (-1)))))
 instance Multiplicative (Exp 'R) where
   one = Con (General (Incl 1))
-  x * y  = Con (General Mult) `App` x `App` y
+  x * y  = Con (General Mult) @@ x @@ y
 instance Division (Exp 'R) where
-  x / y  = Con (General Divi) `App` x `App` y
+  x / y  = Con (General Divi) @@ x @@ y
+instance Scalable (Exp 'R) (Exp 'R) where
+  (*^) = (*)
+instance Ring (Exp 'R) where
+  fromInteger = Con . General . Incl . fromInteger
+
+uniform :: Rational -> Rational -> Exp (('R ⟶ 'R) ⟶ 'R)
+uniform x y = App (Con $ General Uni) (Pair (Con $ General $ Incl x) (Con $ General $ Incl y))
+
+normal :: Rational -> Rational -> Exp (('R ⟶ 'R) ⟶ 'R)
+normal x y = App (Con $ General Nml) (Pair (Con $ General $ Incl x) (Con $ General $ Incl y))
+
+factor :: Exp 'R -> Exp (('Unit ⟶ 'R) ⟶ 'R)
+factor x = Lam (\k -> (k @@ TT) * x)
+
+observe :: Exp 'T -> Exp (('Unit ⟶ 'R) ⟶ 'R)
+observe x = factor (Con (General Indi) @@ x)  
+
+(≥) :: Exp 'R -> Exp 'R -> Exp 'T
+x ≥ y = (Con (Special GTE)) @@ x @@ y
+
+u' :: Exp 'R -> Exp 'U
+u' = App $ Con $ General Utt'
+
+π :: α ∈ κ -> Exp κ -> Exp α
+π Get κ = Snd κ
+π (Weaken i) κ = π i (Fst κ)
+
