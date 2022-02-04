3db21dc0e99be61c1f08168f42eaabda631d050c
Jean-Philippe Bernardy <jeanphilippe.bernardy@gmail.com>
2022-02-04 15:49:17 +0100

    fix model l1 in numeric

--- src/Models/RSA2d.hs
+++ src/Models/RSA2d.hs
@@ -5,82 +5,105 @@
 {-# LANGUAGE RebindableSyntax #-}
 module Models.RSA2d where
 
-import Prelude (Double,Int, ($), (.), fmap, Ord(..), toRational, putStrLn, IO,String,show,unwords,unlines,writeFile)
+import Prelude (Double,Int, ($), (.), fmap, Ord(..), toRational, putStrLn, IO,String,show,unwords,unlines,writeFile,Integer)
 import Algebra.Linear.Chebyshev
-import Algebra.Linear
 import Algebra.Linear.Vector
 import Algebra.Classes
 import Data.Complex
-import Text.Pretty.Math
 import Data.Foldable
+import Control.Applicative
 
 type Ivl = (Double,Double)
 
 type F2 a = Double -> Double -> a
-type P2 a = Polynomial (Polynomial a)
 type S2 a = Samples (Samples a)
+-- type P2 a = Polynomial (Polynomial a)
 
 type C = Complex Double
 
 normal :: Transcendental a => a -> a -> a -> a
 normal μ σ x = recip (σ * sqrt (2 * pi)) * exp (-0.5 * ((μ - x) / σ) ^+ 2)
 
+prior :: Transcendental a => a -> p -> a
+prior h u = normal 68 3 h
+
 groundFun :: (Ord t, Transcendental t) => t -> t -> t
-groundFun h u = if h < u then 0 else
-                normal 68 3 h 
+groundFun h u = if h < u then 0 else prior h u
+                 
 
-size__ = 128
+size__ :: Int
+size__ = 256
 
-ivl = (68-10, 68+10)
+ivlH :: (Double, Double)
+ivlH = (68-10, 68+10)
+
+ivlU :: (Double, Double)
+ivlU = (68-20, 68+10)
 
 ground :: S2 Double
-ground = sample2d size__ ivl ivl (groundFun)
+ground = sample' groundFun
+
+sample' :: F2 Double -> S2 Double
+sample' = sample2d size__ ivlH ivlU
 
 sample2d :: Int -> Ivl -> Ivl -> F2 Double -> S2 Double
-sample2d n (lo1,h1) (l2,h2) f = sample @Double n lo1 h1 $ \x ->
-                               sample @Double n l2 h2 $ \y ->
-                               f x y
+sample2d n (lo1,hi1) (lo2,hi2) f =
+  sample @Double n lo1 hi1 $ \x ->
+  sample @Double n lo2 hi2 $ \y ->
+  f x y
 
-fit2 :: S2 Double -> P2 Double
-fit2 = fmap (fmap realPart) . samplesToPoly @C  . fmap (samplesToPoly @C . fmap (:+ 0) )
+normalizeS :: Samples Double -> Samples Double
+normalizeS v = (recip (integralUnitIvl @Double $ fmap realPart $ samplesToPoly @C $ fmap (:+ 0) v)) *^ v
 
-get :: Int -> Int -> Samples (Samples a) -> a
-get i j m = fromSamples (fromSamples m ! i) ! j
+-- fit2 :: S2 Double -> P2 Double
+-- fit2 = fmap (fmap realPart) . samplesToPoly @C  . fmap (samplesToPoly @C . fmap (:+ 0) )
+
+
+get :: Samples (Samples a) -> Int -> Int -> a
+get m i j = fromSamples (fromSamples m ! i) ! j
 
 pts :: Vec Double
 pts = chebPoints size__
 
-dump :: String -> Samples (Samples Double) -> IO ()
-dump fn x = writeFile fn
+toGnuPlot :: String -> Samples (Samples Double) -> IO ()
+toGnuPlot fn x = writeFile fn
             $   unlines $ fmap (unwords . fmap show) $
             (0 : (toList pts)) :
             [ (pts ! i) : toList (fromSamples x ! i)  | i <- [0..size__] ]
 
-
+normalizeH :: Samples (Samples Double) -> Samples (Samples Double)
 normalizeH = xpose . fmap normalizeS . xpose
+ where
+  xpose :: Samples (Samples Double) -> Samples (Samples Double)
+  xpose m = Samples $ generate (size__+1)  $ \i -> Samples $ generate (size__+1) $ \ j -> get m j i
+
+mkSamples :: (Int -> Int -> a) -> Samples (Samples a)
+mkSamples f = Samples $ generate (size__+1)  $ \i -> Samples $ generate (size__+1) $ \ j -> f i j
+
+normalizeU :: Samples (Samples Double) -> Samples (Samples Double)
 normalizeU = fmap normalizeS
 
 l0 :: Samples (Samples Double)
 l0 = normalizeH ground
 
-xpose :: Samples (Samples Double) -> Samples (Samples Double)
-xpose m = Samples $ generate (size__+1)  $ \i -> Samples $ generate (size__+1) $ \ j -> get j i m
+α :: Natural
+α = 40
 
 s1 :: Samples (Samples Double)
-s1 =  normalizeU l0
+s1 =  normalizeU (fmap (fmap (^+ α)) l0)
 
-lo1 :: Samples (Samples Double)
-lo1 =  normalizeH s1
+l1 :: Samples (Samples Double)
+l1 = normalizeH $ mkSamples $ \h u -> get s1 h u * get (sample' $ prior) h u
 
--- >>> dump "ground.dat" ground
--- >>> dump "l0.dat" l0
--- >>> dump "s1.dat" s1
--- >>> dump "lo1.dat" lo1
+-- >>> toGnuPlot "ground.dat" ground
+-- >>> toGnuPlot "l0.dat" l0
+-- >>> toGnuPlot "s1.dat" s1
+-- >>> toGnuPlot "l1.dat" l1
 
+-- To see this, in gnuplot, run:
+-- set zrange [0:5]
+-- splot 'l1.dat' nonuniform matrix with pm3d
 
-
-normalizeS :: Samples Double -> Samples Double
-normalizeS v = (recip (integralUnitIvl @Double $ fmap realPart $ samplesToPoly @C $ fmap (:+ 0) v)) *^ v
 
 
 
