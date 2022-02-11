92999305b0a23803fb2f68c0cce0ab4e8f7f41e0
Jean-Philippe Bernardy <jeanphilippe.bernardy@gmail.com>
2022-02-11 11:32:41 +0100

    clean

--- src/Models/Field.hs
+++
@@ -1,9 +0,0 @@
-{-# LANGUAGE TypeApplications #-}
-{-# LANGUAGE RankNTypes #-}
-{-# LANGUAGE PatternSynonyms #-}
-{-# LANGUAGE ViewPatterns #-}
-{-# LANGUAGE LambdaCase #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE RebindableSyntax #-}
-
-module Models.Field where
